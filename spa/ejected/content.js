77c9267aba00956761cab35f84cfe4c15743a64c
claxxmoldii <claxxmoldii@users.noreply.github.com>
2021-09-01 11:26:36 +0000

    Deploying to live-test from @ claxxmoldii/claxxmoldii.github.io@fa3413d82f51c7770570e3a581818ec65da32d05 🚀

---
+++ spa/ejected/content.js
@@ -0,0 +1,238 @@
+const allContent = [{
+"pager": 1,
+"path": "/attributions",
+"type": "behindthescenes",
+"filename": "attributions.json",
+"fields": {
+  "title": "attributions",
+  "tagline": "thanks to these awesome superheroes!",
+  "created": "2021.04.29",
+  "component": "attributions"
+}
+
+},{
+"pager": 1,
+"path": "/buildlog",
+"type": "behindthescenes",
+"filename": "buildlog.json",
+"fields": {
+  "title": "build log",
+  "tagline": "how this stuff was put together",
+  "created": "2021.04.28",
+  "component": "buildlog"
+}
+
+},{
+"pager": 1,
+"path": "/issues",
+"type": "behindthescenes",
+"filename": "issues.json",
+"fields": {
+  "title": "issues",
+  "tagline": "quirky stuff and resolutions",
+  "created": "2021.04.29",
+  "component": "issues"
+}
+
+},{
+"pager": 1,
+"path": "/reproduction",
+"type": "behindthescenes",
+"filename": "reproduction.json",
+"fields": {
+  "title": "reproduction sequence",
+  "tagline": "how to reproduce this stuff after plenti update",
+  "created": "2021.05.22",
+  "component": "reproduction"
+}
+
+},{
+"pager": 1,
+"path": "collections/bingewatch",
+"type": "collections",
+"filename": "bingewatch.json",
+"fields": {
+  "title": "binge watch",
+  "tagline": "watch these people!",
+  "created": "2021.04.30",
+  "current": "collections",
+  "collection": "bingewatch",
+  "component": "bingewatch"
+}
+
+},{
+"pager": 1,
+"path": "collections/neattools",
+"type": "collections",
+"filename": "neattools.json",
+"fields": {
+  "title": "neat tools",
+  "tagline": "really neat tools!",
+  "created": "2021.04.30",
+  "current": "collections",
+  "collection": "neattools",
+  "component": "neattools"
+}
+
+},{
+"pager": 1,
+"path": "collections/toolbox",
+"type": "collections",
+"filename": "toolbox.json",
+"fields": {
+  "title": "tool box",
+  "tagline": "really, really useful...",
+  "created": "2021.04.30",
+  "current": "collections",
+  "collection": "toolbox",
+  "component": "toolbox"
+}
+
+},{
+"pager": 1,
+"path": "collections/toolcart",
+"type": "collections",
+"filename": "toolcart.json",
+"fields": {
+  "title": "tool cart",
+  "tagline": "really great stuff to use!",
+  "created": "2021.04.30",
+  "current": "collections",
+  "collection": "toolcart",
+  "component": "toolcart"
+}
+
+},{
+"pager": 1,
+"path": "/",
+"type": "index",
+"filename": "index.json",
+"fields": {
+  "title": "claxx moldii",
+  "created": "2021.04.28",
+  "rebuild": "2021.8.14",
+  "tagline": [
+    "learn something everyday",
+    "to teach is to learn",
+    "discovery is magic",
+    "a student forever",
+    "never stop learning",
+    "learning is fun",
+    "it's never too late to learn something"
+  ],
+  "current": "home"
+}
+
+},{
+"pager": 1,
+"path": "/about",
+"type": "pages",
+"filename": "about.json",
+"fields": {
+  "title": "about claxx",
+  "description": "all about claxx",
+  "created": "2021.04.28",
+  "pic": "minifig-02.jpg",
+  "body": [
+    "loves to draw.",
+    "does technical illustrations pretty well.",
+    "his first job was as a maintenance engineer for a heat treatment shop.",
+    "moved on to designing industrial equipments"
+  ],
+  "body2": [
+    "did his first paid web site gig on adobe golive! yey!",
+    "then found out about web development and loved it.",
+    "learned css and loved it even more!",
+    "currently learning svelte and plenti and having a blast!"
+  ],
+  "body3": [
+    "an okay cook",
+    "an okay fix-it guy.",
+    "still loves to learn new things",
+    "and everyday, for a few brilliant moments, when everything's firing okay in the brain, despite his ADD, does his best to be a decent human being."
+  ],
+  "current": "about"
+}
+
+},{
+"pager": 1,
+"path": "/contact",
+"type": "pages",
+"filename": "contact.json",
+"fields": {
+  "title": "contact me!",
+  "description": "all about claxx",
+  "created": "2021.04.28",
+  "body": [
+    "yeah! is that funky or what...",
+    "a decent human being."
+  ],
+  "current": "contact"
+}
+
+},{
+"pager": 1,
+"path": "/posts",
+"type": "pages",
+"filename": "posts.json",
+"fields": {
+  "title": "posts",
+  "description": "all about claxx",
+  "created": "2021.04.28",
+  "body": [
+    "taking timeout to learn webdesign from code camp",
+    "trashing bad habits to make good ones",
+    "i've been using headers all the wrong way, all these years"
+  ],
+  "current": "posts"
+}
+
+},{
+"pager": 1,
+"path": "posts/littlenap",
+"type": "posts",
+"filename": "littlenap.json",
+"fields": {
+  "title": "Little Nap Coffee Stand serves freshly roasted coffee",
+  "year": "2020",
+  "month": "2020.11",
+  "date": "2020.11.01",
+  "readTime": "4 mins",
+  "author": "claxx moldii",
+  "pic": "littleNap.webp",
+  "caption": "Little Nap Coffee Stand serves freshly roasted coffee | © amabird / Shutterstock.com",
+  "teaser": "It has a laid-back atmosphere and the friendly owner...",
+  "body": [
+    "Just across from Yoyogi Park in Shibuya, you’ll find Little Nap Coffee Stand, a tiny coffee shop with a few extra seats outside. It has a laid-back atmosphere and the friendly owner, Daisuke Hamada, will be more than happy to engage in conversation with visitors. The high-quality beans are roasted on-site, and the <a href='/'>menu</a> includes single-origin drip coffee along with the typical espresso-based drinks, which are prepared on a Synesso machine."
+  ],
+  "tag": "tokyo",
+  "category": "happy",
+  "post": true
+}
+
+},{
+"pager": 1,
+"path": "posts/sbreserve",
+"type": "posts",
+"filename": "sbreserve.json",
+"fields": {
+  "title": "Starbucks Reserve Roastery",
+  "year": "2020",
+  "month": "2020.11",
+  "date": "2020.11.02",
+  "readTime": "4 mins",
+  "author": "claxx moldii",
+  "pic": "sbReserve.webp",
+  "caption": "Starbucks Reserve Roastery Tokyo | © Starbucks Reserve Roastery Tokyo",
+  "teaser": "...the largest Starbucks in the world.",
+  "body": [
+    "Yes, it’s a Starbucks, but not as you know it. Co-designed by architect Kengo Kuma, Nakameguro’s Reserve Roastery is the largest Starbucks in the world. The building incorporates Japanese design elements, such as origami-inspired ceilings and delicate cherry blossoms around the huge, hammered-copper cask rising through the centre of the building. The flowers are inspired by the springtime views over the river just outside, which is lined with sakura trees. On the floors above the roastery and café, the building houses Princi bakery, Teavana and the Arriviamo bar."
+  ],
+  "tag": "kyoto",
+  "category": "tea",
+  "post": true
+}
+
+},];
+
+export default allContent;
