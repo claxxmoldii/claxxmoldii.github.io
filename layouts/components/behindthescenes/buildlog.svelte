7addb04e9b662e3406e7b194bb52e851db74e7c6
claxx moldii <claxxmoldii@users.noreply.github.com>
2021-09-21 11:47:50 +0800

    merged and updated. good commit.

--- layouts/components/behindthescenes/buildlog.svelte
+++ layouts/components/behindthescenes/buildlog.svelte
@@ -65,6 +65,7 @@
     <li>rebuilt from plenti --bare v.0.4.26 <span class="date rebuild">/2021.08.14</span></li>
     <li>update good! move on! yosh! go!<span class="date">/2021.08.14</span></li>
     <li>good save... i think i don't need the env base thing here just yet. commit this!<span class="date">/2021.09.07</span></li>
+    <li>css not loading on refresh, fixed by jim fisk. issue is leading front-slash on link tag inside head.svelte. yey! pull changes from origin and merged with local. woohoo! game on! <span class="date">/2021.09.21</span></li>
   </ul>
 
 <style>
